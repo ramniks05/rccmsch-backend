0fdd9d0d76cc05ea5c714e1a2abffdddfdcfaa3e
satpal <satpal@satpals-MacBook-Air.local>
2026-02-12 14:04:25 +0530

    filePath to url renaming

--- src/main/java/in/gov/manipur/rccms/dto/DocumentAvailableDTO.java
+++ src/main/java/in/gov/manipur/rccms/dto/DocumentAvailableDTO.java
@@ -13,13 +13,14 @@ public class DocumentAvailableDTO {
     private Long documentId;
     private String title;
     private LocalDate publishedOn;
+    private String filePath;
 
 
     public DocumentAvailableDTO(DocumentsAvailable documentsAvailable) {
         this.documentId = documentsAvailable.getDocumentId() != null ? documentsAvailable.getDocumentId() : null;
         this.title = documentsAvailable.getTitle() != null ? documentsAvailable.getTitle() : null;
         this.publishedOn = documentsAvailable.getPublishedOn() != null ? documentsAvailable.getPublishedOn() : null;
-
+        this.filePath = documentsAvailable.getFilePath() != null ? documentsAvailable.getFilePath() : null;
 
     }
 
